69b77410919f9e62e13f6eeef9f737b1fcea8703
Thomas Dion <thomdion05@gmail.com>
2024-04-16 13:21:46 -0400

    tests de home version 1

---
+++ apptrpr02/src/components/__tests__/Home.test.ts
@@ -0,0 +1,50 @@
+import { describe, it, expect, vi } from 'vitest'
+import { mount } from '@vue/test-utils'
+import Home from '../Home.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from '../../router/routes'
+import { useToast } from 'vue-toast-notification'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes
+})
+
+describe('Home', () => {
+    it('Sur envoi du formulaire, doit pouvoir naviguer sur la page de mission.', async () => {
+        router.push('/') // S'assurer de commencer avec une route connue
+        await router.isReady() // Attendre que le routeur soit prêt
+
+        const wrapper = mount(Home, {
+            global: {
+                plugins: [router]
+            }
+        })
+
+        const routerSpy = vi.spyOn(router, 'push')
+
+        // Trouver le lien par son texte et simuler un clic
+        const sendButton = wrapper.find('#sendButton') // Adapter le sélecteur selon ton besoin
+        await sendButton.trigger('click')
+
+        expect(routerSpy).toHaveBeenCalledWith('/')
+    })
+
+
+    it("Sur envoi du formulaire, lorsque nom manquant, une erreur s'affiche.", async () => {
+        const wrapper = mount(Home);
+
+        const nameInput = wrapper.find('#name');
+        await nameInput.setValue('');
+
+        const form = wrapper.find('form');
+        await form.trigger('submit.prevent');
+
+        const toast = wrapper.findComponent({ name: 'VueToastification' }); // Assuming toast is a Vue component
+        expect(toast.exists()).to.be.true;
+
+        // Assert the toast message content
+        expect(toast.text()).to.equal('Veuillez entrer un nom de joueur');
+    });
+})
+
